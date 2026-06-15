d38dfb99722121bdcd98c195d35c55da251c9a9b
joao vitor <jvitor.spaula@gmail.com>
2026-06-15 03:01:19 -0300

    implements listAllCalls and createCall method in controller layer

---
+++ backend/src/controllers/chamado_controller.ts
@@ -0,0 +1,25 @@
+import { ChamadoService } from '../services/chamado_service';
+import { Request, Response } from 'express';
+import {callSchema}  from '../schema/chamadoValidation';
+
+export class ChamadoController {
+    private chamadoService: ChamadoService = new ChamadoService();
+    public async listAllCalls(_req: Request, res: Response): Promise<Response> {
+        try {
+            const response = await this.chamadoService.listAllCalls();
+            return res.status(200).json(response);
+        } catch (error: any) {            
+            return res.status(500).json({ error: error.message });
+        }
+    }
+
+    public async createCall(req: Request, res: Response): Promise<Response> {
+        try {
+            const callValidated = callSchema.parse(req.body);
+            const response = await this.chamadoService.createCall(callValidated);
+            return res.status(201).json(response);
+        } catch (error: any) {
+            return res.status(400).json({ error: error.message });
+        }
+    }
+}
