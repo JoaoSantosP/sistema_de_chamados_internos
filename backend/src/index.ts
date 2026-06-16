074b958ad9f10990dd2aa4956d668a5ab9434411
joao vitor <jvitor.spaula@gmail.com>
2026-06-16 07:46:11 -0300

    implements chamadoRouter in index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import './database/connection';
+import chamadoRouter from './routes/chamado_router';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/chamados', chamadoRouter);
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.send('Hello, World!');
-});
+}); */
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
