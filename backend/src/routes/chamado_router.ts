cb8aa4840e4e8dde980975038028fdce11635b9a
joao vitor <jvitor.spaula@gmail.com>
2026-06-16 07:38:08 -0300

    create routes chamados

---
+++ backend/src/routes/chamado_router.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { ChamadoController } from '../controllers/chamado_controller';
+import { ChamadoService } from '../services/chamado_service';
+import { ChamadoModel } from '../models/chamado_model';
+
+const router = Router();
+
+const chamadoModel = new ChamadoModel();
+const chamadoService = new ChamadoService(chamadoModel);
+const chamadoController = new ChamadoController(chamadoService);
+
+router.get(
+    '/',
+    (req, res) => chamadoController.listAllCalls(req, res)
+);
+
+router.post(
+    '/',
+    (req, res) => chamadoController.createCall(req, res)
+);
+
+export default router;
