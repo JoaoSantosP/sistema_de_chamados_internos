a889cb3be0fc59800a509a43c9afb372f5822009
joao vitor <jvitor.spaula@gmail.com>
2026-06-17 08:02:37 -0300

    add routes responsible

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import './database/connection';
 import chamadoRouter from './routes/chamado_router';
+import responsaveisRouter from './routes/responsaveis_router';
+import cors from 'cors';
+
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use('/chamados', chamadoRouter);
+app.use('/responsaveis', responsaveisRouter);
 
 const port = process.env.PORT || 3000;
 
